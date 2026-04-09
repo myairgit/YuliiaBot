e500c55d695d051ffbb13d534201f7cd27c69a5b
Vadim <myairgit@gmail.com>
2026-04-10 00:09:34 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 import os
 
+from dotenv import load_dotenv
+load_dotenv()
+
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 STRIPE_SECRET = os.getenv("STRIPE_SECRET")
+DOMAIN = os.getenv("DOMAIN")
 
 CURRENT_EVENT_CHAT_ID = int(os.getenv("CURRENT_EVENT_CHAT_ID", "-1001234567890"))
